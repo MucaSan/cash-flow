c99f433fcb875f5008dd638a5c59c152b18ad8fb
svillar <villar.mucasan@gmail.com>
2024-02-05 22:18:11 -0300

    add transactions frontend and database

---
+++ cash-flow-control/src/main/java/controllers/cashflowcontrol/ManageController.java
@@ -0,0 +1,17 @@
+package controllers.cashflowcontrol;
+
+import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.TextField;
+
+public class ManageController {
+
+    private TextField   textName;
+    private TextField   textSource;
+    private TextField   textAmount;
+    private ComboBox<String> comboSession;
+    private ComboBox<String> comboPayment;
+    private Button  buttonCreate;
+    private Button buttonGoBack;
+
+}
