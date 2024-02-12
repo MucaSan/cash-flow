c6c99b94c735e0ecbf360eff919d8c97a9e4f6a8
svillar <villar.mucasan@gmail.com>
2024-02-12 18:55:01 -0300

    add functionality for the edit and delete tableview

--- cash-flow-control/src/main/java/controllers/cashflowcontrol/SessionMenuController.java
+++ cash-flow-control/src/main/java/controllers/cashflowcontrol/SessionMenuController.java
@@ -1,7 +1,11 @@
 package controllers.cashflowcontrol;
+import com.almasb.fxgl.entity.action.Action;
 import generalPurposesClasses.cashflowcontrol.Session;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
+import javafx.event.Event;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
@@ -11,6 +15,7 @@ import javafx.scene.control.TextField;
 import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.HBox;
 
@@ -44,13 +49,25 @@ public class SessionMenuController implements Initializable {
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
         ObservableList<Session> listData = FXCollections.observableArrayList();
-        ImageView test = new ImageView(imgEdit.getImage());
-        ImageView test2 = new ImageView(imgDelete.getImage());
-        test.setFitHeight(25);
-        test.setFitWidth(25);
-        test2.setFitHeight(25);
-        test2.setFitWidth(25);
-        listData.add(new Session(1, "test",new HBox(test,test2)));
+        for (int i = 0; i <= 2; i++){
+            final int j = i;
+            ImageView test = new ImageView(){
+                {
+                    setId(String.format("%d", j));
+                }
+                {
+                    setImage(imgEdit.getImage());
+                }
+            };
+            ImageView test2 = new ImageView(imgDelete.getImage());
+            test.setFitHeight(25);
+            test.setOnMouseClicked(mouseEvent -> System.out.println(tableSession.getItems().get(Integer.parseInt(test.getId())).getId()));
+            test.setFitWidth(25);
+            test2.setFitHeight(25);
+            test2.setFitWidth(25);
+            listData.add(new Session(i, "test",new HBox(test,test2)));
+        }
+
         colID.setCellValueFactory(new PropertyValueFactory<>("id"));
         colName.setCellValueFactory(new PropertyValueFactory<>("name"));
         colAction.setCellValueFactory(new PropertyValueFactory<>("actions"));
